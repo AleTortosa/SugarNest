ebd91ab5ff13b1c1252ec12dbd868f0da02d08d8
AleTortosa <aletorta7@gmail.com>
2026-03-11 11:14:32 +0100

    Navbar responsive y mobile-first, estilo parcial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,34 +6,37 @@ export function Navbar() {
     const [menu, setMenu] = useState(false)
 
     const navClassName = menu
-    ? 'navbar'
-    : 'inactivo'
+    ? 'navMenu activo'
+    : 'navMenu'
 
-    const text = menu
-    ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" style={{ width: '24px', height: '24px' }}>
+    const icono = menu
+    ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '48px', height: '48px' }}>
   <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
 </svg>
-    : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" style={{ width: '24px', height: '24px' }}>
+    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '48px', height: '48px' }}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
 
 
     function toggleMenu(){
-        setMenu(!menu)
+        setMenu(prev => !prev)
     }
     
     return (
         <Fragment>
-            <button onClick={toggleMenu}>{text}</button>
-            <nav className={navClassName}>
-                <ul>
-                    <li><Link to="/">Inicio</Link></li>
-                    <li><Link to="/menu">Menú</Link></li>
-                    <li><Link to="/contact">Contacto</Link></li>
+            <nav className='navbar'>
+                <div className='navTop'>
+                    <button className='hamburguesa' onClick={toggleMenu}>{icono}</button>
+                    <img className='logo' src="../assets/logo.png" alt="Logo" />
+                </div>
+
+                <ul className={navClassName}>
+                    <li className='filaMenu'><Link to="/" onClick={toggleMenu}>Inicio</Link></li>
+                    <li className='filaMenu'><Link to="/menu" onClick={toggleMenu}>Menú</Link></li>
+                    <li className='logo-escritorio'><img src="../assets/logo.png" alt="Logo" className='logo'/></li>
+                    <li className='filaMenu'><Link to="/contact" onClick={toggleMenu}>Contacto</Link></li>
                 </ul>
             </nav>
-
-            <img src="../assets/logo.png" alt="Logo" className='logo' />
         </Fragment>
     )
 }
