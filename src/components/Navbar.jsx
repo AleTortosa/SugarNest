327061722ff05e6687cb604d4e9fec5b596a91ed
AleTortosa <aletorta7@gmail.com>
2026-03-11 13:48:41 +0100

    Mejoras visuales navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ export function Navbar() {
     : 'navMenu'
 
     const icono = menu
-    ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '48px', height: '48px' }}>
+    ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '35px', height: '35px' }}>
   <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
 </svg>
-    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '48px', height: '48px' }}>
+    : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" style={{ width: '35px', height: '35px' }}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
 
@@ -30,12 +30,17 @@ export function Navbar() {
             <nav className='navbar'>
                 <div className='navTop'>
                     <button className='hamburguesa' aria-label='Menu' onClick={toggleMenu}>{icono}</button>
-                    <img className='logo' src={logo} alt="Logo" />
+                    <Link to="/">
+                        <img src={logo} alt="Logo" className='logo'/>
+                    </Link>
                 </div>
 
                 <ul className={navClassName}>
                     <li className='filaMenu'>
-                        <Link to="/" onClick={toggleMenu}>Inicio</Link>
+                        <Link to="/" onClick={toggleMenu}>P&aacute;gina principal</Link>
+                    </li>      
+                    <li className='filaMenu'>
+                        <Link to="/" onClick={toggleMenu}>Proyectos</Link>
                     </li>                                         
                         
                     <li className='logo-escritorio'>
@@ -45,8 +50,11 @@ export function Navbar() {
                     </li>
 
                     <li className='filaMenu'>
+                        <Link to="/menu" onClick={toggleMenu}>Interior tartas</Link>
+                    </li>     
+                    <li className='filaMenu'>
                         <Link to="/contact" onClick={toggleMenu}>Contacto</Link>
-                    </li>                        
+                    </li>                    
                 </ul>
             </nav>
         </Fragment>
