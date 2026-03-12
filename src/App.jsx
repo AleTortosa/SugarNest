827aeba62cf5168871c720873f4c78af41d34dd5
AleTortosa <aletorta7@gmail.com>
2026-03-12 18:02:52 +0100

    Corrección de fallos menores de css y posición de página

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ScrollToTop } from './components/ScrollToTop.jsx'
+
 import { Navbar } from './components/Navbar.jsx'
+import { Footer } from './components/Footer.jsx'
 import { Home } from './pages/home.jsx'
 import { Contact } from './pages/Contact.jsx'
 import { Menu } from './pages/Menu.jsx'
-import { Footer } from './components/Footer.jsx'
 import { AvisoLegal } from './pages/legal/AvisoLegal.jsx'
 import { Privacidad } from './pages/legal/Privacidad.jsx'
 import { Cookies } from './pages/legal/Cookies.jsx'
@@ -11,6 +13,9 @@ import { Cookies } from './pages/legal/Cookies.jsx'
 export function App() {
     return (
         <BrowserRouter>
+
+            <ScrollToTop/>
+            
             <div className='app-container'>
                 <Navbar />
 
