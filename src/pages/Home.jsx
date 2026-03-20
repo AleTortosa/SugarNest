8883d6282ffab9cf55233e7f54fd5f7f3911b964
AleTortosa <aletorta7@gmail.com>
2026-03-20 18:46:44 +0100

    Implementación de un segundo carrusel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,16 @@ import roscon3 from '../assets/images/imagen-carrusel/roscon3.jpeg'
 import roscon4 from '../assets/images/imagen-carrusel/roscon4.jpeg'
 
 /*Import fotos carrusel*/
+import tarta1 from '../assets/images/imagen-carrusel/tarta-amor.jpeg'
+import tarta2 from '../assets/images/imagen-carrusel/tarta-chocofresa.jpeg'
+import tarta3 from '../assets/images/imagen-carrusel/tarta-dragon.jpeg'
+import tarta4 from '../assets/images/imagen-carrusel/tarta-elegante.jpeg'
+import tarta5 from '../assets/images/imagen-carrusel/tarta-foca.jpeg'
+import tarta6 from '../assets/images/imagen-carrusel/tarta-harrypoter.jpeg'
+import tarta7 from '../assets/images/imagen-carrusel/tarta-osito.jpeg'
+import tarta8 from '../assets/images/imagen-carrusel/tarta-ranas.jpeg'
+import tarta9 from '../assets/images/imagen-carrusel/tarta-rural.jpeg'
+import tarta10 from '../assets/images/imagen-carrusel/tarta-taxi.jpeg'
 
 export function Home() {
     const imagenes_festivo = [
@@ -17,6 +27,16 @@ export function Home() {
         { src: roscon3 },
         { src: roscon4 }
     ];
+
+    const imagenes_carrusel = [
+        { src: tarta1 },
+        { src: tarta2 },
+        { src: tarta7 },
+        { src: tarta8 },
+        { src: tarta5 },
+        { src: tarta10 },
+    ];
+
 
     return (
         <Fragment>
@@ -33,6 +53,14 @@ export function Home() {
                 <Carrusel imagenes={imagenes_festivo} />
 
                 <button className="boton-principal">Ver toda la colección</button>
+            </section>
+
+            <section className="seleccion-festivo">
+                    <h2>Nuestras mejores tartas</h2>
+                
+                <Carrusel imagenes={imagenes_carrusel} />
+
+                <button className="boton-principal">Ver todas las tartas</button>
             </section>
         </Fragment >
     )
