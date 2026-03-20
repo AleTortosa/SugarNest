67af696b598c674e159db1f558ed961cda1f74de
AleTortosa <aletorta7@gmail.com>
2026-03-20 12:41:32 +0100

    Componente Carrusel creado

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,39 @@
+import { Fragment, useState } from "react"
+
+export function Carrusel({ imagenes }) {
+    const [activeImg, setActiveImg] = useState(null);
+    const imagenes_local = [...imagenes, ...imagenes]
+
+
+    const claseActivaImagen = activeImg
+        ? "zoom-img"
+        : "zoom-img cerrar-foto";
+
+    function fotoActiva(srcFoto) {
+        setActiveImg(srcFoto);
+    }
+
+
+    return (
+        <Fragment>
+            <div className={claseActivaImagen}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6" style={{ width: '54px', height: '54px' }} onClick={() => fotoActiva(null)}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+
+                <img src={activeImg}></img>
+            </div>
+
+            <div className="home-carrusel-container">
+                <div className="home-carrusel-track">
+                    {
+                        imagenes_local.map(({ src }, index) => (
+                            <div className="home-carrusel-item" key={`${src}-${index}`}>
+                                <img alt={src} src={src} onClick={() => fotoActiva(src)} />
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </Fragment>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { Fragment, useState } from "react"
 import "../styles/home.css"
+import { Carrusel } from "../components/Carrusel.jsx"
+
 /*Import fotos carrusel festivos*/
 import roscon1 from '../assets/images/imagen-carrusel/roscon.jpeg'
 import roscon2 from '../assets/images/imagen-carrusel/roscon2.jpeg'
@@ -9,15 +11,12 @@ import roscon4 from '../assets/images/imagen-carrusel/roscon4.jpeg'
 /*Import fotos carrusel*/
 
 export function Home() {
-    const [activeImg, setActiveImg] = useState(null);
-
-    const claseActivaImagen = activeImg
-    ? "zoom-img"
-    : "zoom-img cerrar-foto";
-
-    function fotoActiva(srcFoto){
-        setActiveImg(srcFoto);
-    }
+    const imagenes_festivo = [
+        { src: roscon1 },
+        { src: roscon2 },
+        { src: roscon3 },
+        { src: roscon4 }
+    ];
 
     return (
         <Fragment>
@@ -29,48 +28,11 @@ export function Home() {
             </section>
 
             <section className="seleccion-festivo">
-                <div className="festivo-content">
                     <h2>Selección navidad</h2>
-                </div>
+                
+                <Carrusel imagenes={imagenes_festivo} />
 
-                <div className={claseActivaImagen}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" style={{ width: '70px', height: '70px' }} onClick={() => fotoActiva(null)}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-
-                    <img src={activeImg}></img>
-                </div>
-
-                <div className="home-carrusel-container">
-                    <div className="home-carrusel-track">
-                        <div className="home-carrusel-item">
-                            <img alt="foto 1" src={roscon1} onClick={() => fotoActiva(roscon1)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 2" src={roscon2} onClick={() => fotoActiva(roscon2)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 3" src={roscon3} onClick={() => fotoActiva(roscon3)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 4" src={roscon4} onClick={() => fotoActiva(roscon4)} />
-                        </div>
-
-
-                        <div className="home-carrusel-item">
-                            <img alt="foto 1" src={roscon1} onClick={() => fotoActiva(roscon1)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 2" src={roscon2} onClick={() => fotoActiva(roscon2)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 3" src={roscon3} onClick={() => fotoActiva(roscon3)} />
-                        </div>
-                        <div className="home-carrusel-item">
-                            <img alt="foto 4" src={roscon4} onClick={() => fotoActiva(roscon4)} />
-                        </div>
-                    </div>
-                </div>
+                <button className="boton-principal">Ver toda la colección</button>
             </section>
         </Fragment >
     )
