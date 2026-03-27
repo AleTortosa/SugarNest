924c719e41e02f968ae2357361f04a7df0d938f0
AleTortosa <aletorta7@gmail.com>
2026-03-27 11:33:49 +0100

    Flechas movilidad en biblioteca

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -10,23 +10,38 @@
 import { Fragment, useEffect, useState } from 'react';
 
 export function Biblioteca({ imagenes }) {
-    const [activeImg, setActiveImg] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const claseActivaImagen = activeImg
+    const claseActivaImagen = activeIndex !== null
         ? "zoom-img"
         : "zoom-img cerrar-foto";
 
-    function fotoActiva(srcFoto) {
-        setActiveImg(srcFoto);
+    function abrirFoto(srcFoto) {
+        setActiveIndex(srcFoto);
+    }
+
+    function cerrarFoto() {
+        setActiveIndex(null);
+    }
+
+    function fotoSiguiente(){
+        setActiveIndex((prev) => (prev + 1) % imagenes.length);
+    }
+
+    function fotoAnterior(){
+        setActiveIndex((prev) => (prev - 1 + imagenes.length) % imagenes.length);
     }
 
     useEffect(() => {
-        if (!activeImg) return;
+        if (activeIndex === null) return;
 
         const handler = (e) => {
-            if (e.key === "Escape") {
-                setActiveImg(null);
-            }
+            if (e.key === "Escape")
+                cerrarFoto();
+            if (e.key === "ArrowRight")
+                fotoSiguiente();
+            if (e.key === "ArrowLeft")
+                fotoAnterior();
         };
 
         window.addEventListener("keydown", handler);
@@ -34,16 +49,29 @@ export function Biblioteca({ imagenes }) {
         return () => {
             window.removeEventListener("keydown", handler);
         };
-    }, [activeImg]);
+    }, [activeIndex]);
 
     return (
         <Fragment>
             <div className={claseActivaImagen}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 equis" style={{ width: '54px', height: '54px' }} onClick={() => fotoActiva(null)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 equis" style={{ width: '54px', height: '54px' }} onClick={() => cerrarFoto()}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
 
-                <img src={activeImg}></img>
+                <img src={imagenes[activeIndex]?.src} />
+
+                <div className='flechas'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='flecha' style={{ width: '70px', height: '70px' }} onClick={() => fotoAnterior()}>
+                        <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='flecha' style={{ width: '70px', height: '70px' }} onClick={() => fotoSiguiente()}>
+                        <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+                    </svg>
+                </div>
+
+
+
             </div>
 
             <div className='biblioteca-contenedor'>
@@ -51,7 +79,7 @@ export function Biblioteca({ imagenes }) {
                     {
                         imagenes.map(({ src }, index) => (
                             <div className="biblioteca-foto" key={`${src}-${index}`}>
-                                <img alt={src} src={src} onClick={() => fotoActiva(src)} />
+                                <img alt={src} src={src} onClick={() => abrirFoto(index)} />
                             </div>
                         ))}
                 </div>
